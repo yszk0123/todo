826ab448ad81f07b8057bb52678fd0e4f02c27f4
Yuji Suzuki <y0suzuki0123@gmail.com>
2020-06-21 18:46:34 +0900

    Add fragment-matcher

--- lib/apollo/createApolloClient.ts
+++ lib/apollo/createApolloClient.ts
@@ -1,8 +1,11 @@
 import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+import introspectionResult from '../graphql/__generated__/introspectionResult';
 
 export function createApolloClient() {
   const client = new ApolloClient({
-    cache: new InMemoryCache(),
+    cache: new InMemoryCache({
+      possibleTypes: introspectionResult.__schema,
+    }),
     link: new HttpLink({
       uri: '/api/graphql',
     }),
