793de6551543a8061262e0d3ada932408169624d
Yuji Suzuki <y0suzuki0123@gmail.com>
2020-06-28 21:35:53 +0900

    Update graphql schema of TodosPage

--- client/graphql/__generated__/TodosPage.graphql.tsx
+++ client/graphql/__generated__/TodosPage.graphql.tsx
@@ -4,7 +4,9 @@ import gql from 'graphql-tag';
 import * as ApolloReactCommon from '@apollo/client';
 import * as ApolloReactHooks from '@apollo/client';
 
-export type TodosPageQueryVariables = Types.Exact<{ [key: string]: never; }>;
+export type TodosPageQueryVariables = Types.Exact<{
+  categoryId: Types.Scalars['Int'];
+}>;
 
 
 export type TodosPageQuery = (
@@ -12,10 +14,18 @@ export type TodosPageQuery = (
   & { me?: Types.Maybe<(
     { __typename?: 'User' }
     & Pick<Types.User, 'id'>
-  )>, todos?: Types.Maybe<Array<(
-    { __typename?: 'Todo' }
-    & Pick<Types.Todo, 'id' | 'text'>
-  )>> }
+  )>, category?: Types.Maybe<(
+    { __typename?: 'Category' }
+    & Pick<Types.Category, 'id' | 'name'>
+    & { todos: Array<(
+      { __typename?: 'Todo' }
+      & Pick<Types.Todo, 'id' | 'text'>
+      & { tags: Array<(
+        { __typename?: 'Tag' }
+        & Pick<Types.Tag, 'id' | 'name'>
+      )> }
+    )> }
+  )> }
 );
 
 export type CreateOneTodoMutationVariables = Types.Exact<{
@@ -59,13 +69,21 @@ export type UpdateTodoMutation = (
 
 
 export const TodosPageDocument = gql`
-    query TodosPage {
+    query TodosPage($categoryId: Int!) {
   me {
     id
   }
-  todos {
+  category(id: $categoryId) {
     id
-    text
+    name
+    todos {
+      id
+      text
+      tags {
+        id
+        name
+      }
+    }
   }
 }
     `;
@@ -82,6 +100,7 @@ export const TodosPageDocument = gql`
  * @example
  * const { data, loading, error } = useTodosPageQuery({
  *   variables: {
+ *      categoryId: // value for 'categoryId'
  *   },
  * });
  */
--- client/graphql/__generated__/baseTypes.ts
+++ client/graphql/__generated__/baseTypes.ts
@@ -13,6 +13,72 @@ export type Scalars = {
   Json: any;
 };
 
+export type Category = {
+  __typename?: 'Category';
+  createdAt: Scalars['DateTime'];
+  id: Scalars['Int'];
+  name: Scalars['String'];
+  owner: User;
+  tags: Array<Tag>;
+  todos: Array<Todo>;
+};
+
+
+export type CategoryTagsArgs = {
+  after?: Maybe<TagWhereUniqueInput>;
+  before?: Maybe<TagWhereUniqueInput>;
+  first?: Maybe<Scalars['Int']>;
+  last?: Maybe<Scalars['Int']>;
+};
+
+
+export type CategoryTodosArgs = {
+  after?: Maybe<TodoWhereUniqueInput>;
+  before?: Maybe<TodoWhereUniqueInput>;
+  first?: Maybe<Scalars['Int']>;
+  last?: Maybe<Scalars['Int']>;
+};
+
+export type CategoryCreateManyWithoutOwnerInput = {
+  connect?: Maybe<Array<CategoryWhereUniqueInput>>;
+  create?: Maybe<Array<CategoryCreateWithoutOwnerInput>>;
+};
+
+export type CategoryCreateManyWithoutTagsInput = {
+  connect?: Maybe<Array<CategoryWhereUniqueInput>>;
+  create?: Maybe<Array<CategoryCreateWithoutTagsInput>>;
+};
+
+export type CategoryCreateOneWithoutTodosInput = {
+  connect?: Maybe<CategoryWhereUniqueInput>;
+  create?: Maybe<CategoryCreateWithoutTodosInput>;
+};
+
+export type CategoryCreateWithoutOwnerInput = {
+  createdAt?: Maybe<Scalars['DateTime']>;
+  name: Scalars['String'];
+  tags?: Maybe<TagCreateManyWithoutCategoriesInput>;
+  todos?: Maybe<TodoCreateManyWithoutCategoryInput>;
+};
+
+export type CategoryCreateWithoutTagsInput = {
+  createdAt?: Maybe<Scalars['DateTime']>;
+  name: Scalars['String'];
+  owner: UserCreateOneWithoutCategoriesInput;
+  todos?: Maybe<TodoCreateManyWithoutCategoryInput>;
+};
+
+export type CategoryCreateWithoutTodosInput = {
+  createdAt?: Maybe<Scalars['DateTime']>;
+  name: Scalars['String'];
+  owner: UserCreateOneWithoutCategoriesInput;
+  tags?: Maybe<TagCreateManyWithoutCategoriesInput>;
+};
+
+export type CategoryWhereUniqueInput = {
+  id?: Maybe<Scalars['Int']>;
+};
+
 
 export type DeleteTodoInput = {
   id: Scalars['Int'];
@@ -43,8 +109,16 @@ export type MutationUpdateTodoArgs = {
 
 export type Query = {
   __typename?: 'Query';
+  categories?: Maybe<Array<Category>>;
+  category?: Maybe<Category>;
   me?: Maybe<User>;
+  tags?: Maybe<Array<Tag>>;
   todos?: Maybe<Array<Todo>>;
+};
+
+
+export type QueryCategoryArgs = {
+  id: Scalars['Int'];
 };
 
 export enum Role {
@@ -52,17 +126,131 @@ export enum Role {
   User = 'USER'
 }
 
+export type Tag = {
+  __typename?: 'Tag';
+  categories: Array<Category>;
+  createdAt: Scalars['DateTime'];
+  id: Scalars['Int'];
+  name: Scalars['String'];
+  owner: User;
+  todos: Array<Todo>;
+};
+
+
+export type TagCategoriesArgs = {
+  after?: Maybe<CategoryWhereUniqueInput>;
+  before?: Maybe<CategoryWhereUniqueInput>;
+  first?: Maybe<Scalars['Int']>;
+  last?: Maybe<Scalars['Int']>;
+};
+
+
+export type TagTodosArgs = {
+  after?: Maybe<TodoWhereUniqueInput>;
+  before?: Maybe<TodoWhereUniqueInput>;
+  first?: Maybe<Scalars['Int']>;
+  last?: Maybe<Scalars['Int']>;
+};
+
+export type TagCreateManyWithoutCategoriesInput = {
+  connect?: Maybe<Array<TagWhereUniqueInput>>;
+  create?: Maybe<Array<TagCreateWithoutCategoriesInput>>;
+};
+
+export type TagCreateManyWithoutOwnerInput = {
+  connect?: Maybe<Array<TagWhereUniqueInput>>;
+  create?: Maybe<Array<TagCreateWithoutOwnerInput>>;
+};
+
+export type TagCreateManyWithoutTodosInput = {
+  connect?: Maybe<Array<TagWhereUniqueInput>>;
+  create?: Maybe<Array<TagCreateWithoutTodosInput>>;
+};
+
+export type TagCreateWithoutCategoriesInput = {
+  createdAt?: Maybe<Scalars['DateTime']>;
+  name: Scalars['String'];
+  owner: UserCreateOneWithoutTagsInput;
+  todos?: Maybe<TodoCreateManyWithoutTagsInput>;
+};
+
+export type TagCreateWithoutOwnerInput = {
+  categories?: Maybe<CategoryCreateManyWithoutTagsInput>;
+  createdAt?: Maybe<Scalars['DateTime']>;
+  name: Scalars['String'];
+  todos?: Maybe<TodoCreateManyWithoutTagsInput>;
+};
+
+export type TagCreateWithoutTodosInput = {
+  categories?: Maybe<CategoryCreateManyWithoutTagsInput>;
+  createdAt?: Maybe<Scalars['DateTime']>;
+  name: Scalars['String'];
+  owner: UserCreateOneWithoutTagsInput;
+};
+
+export type TagWhereUniqueInput = {
+  id?: Maybe<Scalars['Int']>;
+};
+
 export type Todo = {
   __typename?: 'Todo';
   author: User;
   authorId: Scalars['Int'];
+  category: Category;
   createdAt: Scalars['DateTime'];
   id: Scalars['Int'];
+  tags: Array<Tag>;
   text: Scalars['String'];
+};
+
+
+export type TodoTagsArgs = {
+  after?: Maybe<TagWhereUniqueInput>;
+  before?: Maybe<TagWhereUniqueInput>;
+  first?: Maybe<Scalars['Int']>;
+  last?: Maybe<Scalars['Int']>;
 };
 
 export type TodoCreateInput = {
   author: UserCreateOneWithoutTodosInput;
+  category: CategoryCreateOneWithoutTodosInput;
+  createdAt?: Maybe<Scalars['DateTime']>;
+  tags?: Maybe<TagCreateManyWithoutTodosInput>;
+  text: Scalars['String'];
+};
+
+export type TodoCreateManyWithoutAuthorInput = {
+  connect?: Maybe<Array<TodoWhereUniqueInput>>;
+  create?: Maybe<Array<TodoCreateWithoutAuthorInput>>;
+};
+
+export type TodoCreateManyWithoutCategoryInput = {
+  connect?: Maybe<Array<TodoWhereUniqueInput>>;
+  create?: Maybe<Array<TodoCreateWithoutCategoryInput>>;
+};
+
+export type TodoCreateManyWithoutTagsInput = {
+  connect?: Maybe<Array<TodoWhereUniqueInput>>;
+  create?: Maybe<Array<TodoCreateWithoutTagsInput>>;
+};
+
+export type TodoCreateWithoutAuthorInput = {
+  category: CategoryCreateOneWithoutTodosInput;
+  createdAt?: Maybe<Scalars['DateTime']>;
+  tags?: Maybe<TagCreateManyWithoutTodosInput>;
+  text: Scalars['String'];
+};
+
+export type TodoCreateWithoutCategoryInput = {
+  author: UserCreateOneWithoutTodosInput;
+  createdAt?: Maybe<Scalars['DateTime']>;
+  tags?: Maybe<TagCreateManyWithoutTodosInput>;
+  text: Scalars['String'];
+};
+
+export type TodoCreateWithoutTagsInput = {
+  author: UserCreateOneWithoutTodosInput;
+  category: CategoryCreateOneWithoutTodosInput;
   createdAt?: Maybe<Scalars['DateTime']>;
   text: Scalars['String'];
 };
@@ -92,16 +280,46 @@ export type UserTodosArgs = {
   last?: Maybe<Scalars['Int']>;
 };
 
+export type UserCreateOneWithoutCategoriesInput = {
+  connect?: Maybe<UserWhereUniqueInput>;
+  create?: Maybe<UserCreateWithoutCategoriesInput>;
+};
+
+export type UserCreateOneWithoutTagsInput = {
+  connect?: Maybe<UserWhereUniqueInput>;
+  create?: Maybe<UserCreateWithoutTagsInput>;
+};
+
 export type UserCreateOneWithoutTodosInput = {
   connect?: Maybe<UserWhereUniqueInput>;
   create?: Maybe<UserCreateWithoutTodosInput>;
 };
 
-export type UserCreateWithoutTodosInput = {
+export type UserCreateWithoutCategoriesInput = {
   createdAt?: Maybe<Scalars['DateTime']>;
   email: Scalars['String'];
   name: Scalars['String'];
   role?: Maybe<Role>;
+  tags?: Maybe<TagCreateManyWithoutOwnerInput>;
+  todos?: Maybe<TodoCreateManyWithoutAuthorInput>;
+};
+
+export type UserCreateWithoutTagsInput = {
+  categories?: Maybe<CategoryCreateManyWithoutOwnerInput>;
+  createdAt?: Maybe<Scalars['DateTime']>;
+  email: Scalars['String'];
+  name: Scalars['String'];
+  role?: Maybe<Role>;
+  todos?: Maybe<TodoCreateManyWithoutAuthorInput>;
+};
+
+export type UserCreateWithoutTodosInput = {
+  categories?: Maybe<CategoryCreateManyWithoutOwnerInput>;
+  createdAt?: Maybe<Scalars['DateTime']>;
+  email: Scalars['String'];
+  name: Scalars['String'];
+  role?: Maybe<Role>;
+  tags?: Maybe<TagCreateManyWithoutOwnerInput>;
 };
 
 export type UserWhereUniqueInput = {
