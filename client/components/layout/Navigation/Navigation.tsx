2ab49686098ac5f72c02079ac75f7880bf9dd357
Yuji Suzuki <y0suzuki0123@gmail.com>
2020-07-05 00:33:49 +0900

    Extract NavigationLink

--- client/components/layout/Navigation/Navigation.tsx
+++ client/components/layout/Navigation/Navigation.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import NextLink from 'next/link';
-import { Link, Flex, Text, Box } from 'rebass';
+import { Flex, Text, Box } from 'rebass';
 import { Avatar } from './Avatar';
+import { NavigationLink } from './NavigationLink';
 
 export const Navigation: React.FunctionComponent<{
   hasSession: boolean;
@@ -10,33 +10,19 @@ export const Navigation: React.FunctionComponent<{
 }> = ({ hasSession, username, avatarUrl }) => {
   return (
     <Flex px={2} color="white" bg="black" alignItems="center">
-      <NextLink href="/" passHref>
-        <Link variant="nav">
-          <Text p={2} fontWeight="bold">
-            Todo
-          </Text>
-        </Link>
-      </NextLink>
+      <NavigationLink href="/" text="Todo" />
       <Box>
-        <NextLink href="/categories" passHref>
-          <Link variant="nav">Categories</Link>
-        </NextLink>
-        <NextLink href="/tags" passHref>
-          <Link variant="nav">Tags</Link>
-        </NextLink>
+        <NavigationLink href="/categories" text="Categories" />
+        <NavigationLink href="/tags" text="Tags" />
       </Box>
       <Box mx="auto" />
       {username && <Text p={2}>{username}</Text>}
       {avatarUrl && <Avatar url={avatarUrl} alt={username || 'avatar'} />}
       <Box p={2}>
         {hasSession ? (
-          <NextLink href="/api/auth/signout" passHref>
-            <Link variant="nav">Sign out</Link>
-          </NextLink>
+          <NavigationLink href="/api/auth/signout" text="Sign out" />
         ) : (
-          <NextLink href="/api/auth/signin" passHref>
-            <Link variant="nav">Sign in</Link>
-          </NextLink>
+          <NavigationLink href="/api/auth/signin" text="Sign in" />
         )}
       </Box>
     </Flex>
