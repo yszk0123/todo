4fa8e82d8ec13da411dd49c0487f7bb8616a0333
Yuji Suzuki <y0suzuki0123@gmail.com>
2020-07-05 13:26:38 +0900

    Add prop headerText

--- client/components/layout/List/List.tsx
+++ client/components/layout/List/List.tsx
@@ -1,12 +1,38 @@
 import React from 'react';
-import { Box } from 'rebass';
+import { Box, Flex, Text } from 'rebass';
 
 import { stopPropagation } from '../../../handlers/stopPropagation';
-import { EmptyProps } from '../../../viewModels/EmptyProps';
 
-export const List: React.FunctionComponent<EmptyProps> = ({ children }) => {
+type Props = {
+  isFirst?: boolean;
+  headerText?: string;
+};
+
+export const List: React.FunctionComponent<Props> = ({
+  headerText,
+  isFirst = false,
+  children,
+}) => {
+  const hadHeader = !!headerText;
+  const extraMarginTop = isFirst ? 2 : 0;
+
   return (
-    <Box mt={1} onClick={stopPropagation}>
+    <Box
+      fontSize={2}
+      mt={(hadHeader ? 3 : 1) + extraMarginTop}
+      pt={hadHeader ? 2 : 0}
+      sx={{ boxShadow: hadHeader ? 1 : undefined, position: 'relative' }}
+      onClick={stopPropagation}
+    >
+      {hadHeader && (
+        <Flex
+          justifyContent="flex-end"
+          sx={{ position: 'absolute', top: -2, right: 2 }}
+          width={1}
+        >
+          <Text color="gray">{headerText}</Text>
+        </Flex>
+      )}
       {children}
     </Box>
   );
