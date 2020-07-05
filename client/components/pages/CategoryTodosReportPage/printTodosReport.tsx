2c4772081f5840aee61e90c47cfda5d174f2cf40
Yuji Suzuki <y0suzuki0123@gmail.com>
2020-07-05 18:49:43 +0900

    Improve todos report

--- client/components/pages/CategoryTodosReportPage/printTodosReport.tsx
+++ client/components/pages/CategoryTodosReportPage/printTodosReport.tsx
@@ -1,16 +1,35 @@
+import { TodoStatus } from '../../../graphql/__generated__/baseTypes';
 import {
   CategoryTodosReportPageTagFragment,
   CategoryTodosReportPageTodoFragment,
 } from '../../../graphql/__generated__/CategoryTodosReportPage.graphql';
 import { printTodoStatus } from '../../../viewModels/TodoStatusVM';
 
+const statusToIndex = {
+  [TodoStatus.Todo]: 0,
+  [TodoStatus.InProgress]: 1,
+  [TodoStatus.Waiting]: 2,
+  [TodoStatus.Done]: 3,
+};
+
+const TIME_RE = /^(\d{2}:\d{2})[-~]/;
+const TIME_WHOLE_RE = /^(\d{2}:\d{2}[-~])(.*)$/;
+
+function getTime(text: string): number {
+  const match = TIME_RE.exec(text);
+  return match && match[1] ? Number(match[1].replace(/:/, '')) : 0;
+}
+
 export function printTodosReport(
   todos: CategoryTodosReportPageTodoFragment[],
   tags: CategoryTodosReportPageTagFragment[]
 ): string {
   const tagsString = tags.map((tag) => tag.name).join(', ');
+  const tasks = todos.filter((todo) => !TIME_RE.test(todo.text));
+  const schedules = todos.filter((todo) => TIME_RE.test(todo.text));
 
-  const todosString = todos
+  const tasksString = tasks
+    .sort((a, b) => statusToIndex[a.status] - statusToIndex[b.status])
     .map((todo) => {
       const text = todo.text;
       const tagNames = todo.tags.map((tag) => tag.name);
@@ -20,7 +39,19 @@ export function printTodosReport(
     })
     .join('\n');
 
-  const text = [tagsString, todosString].join('\n');
+  const schedulesString = schedules
+    .sort((a, b) => getTime(a.text) - getTime(b.text))
+    .map((todo) => {
+      const text = todo.text;
+      const tagNames = todo.tags.map((tag) => tag.name);
+      const tags = tagNames.length ? `${tagNames.join(', ')}: ` : '';
+      const match = TIME_WHOLE_RE.exec(todo.text);
+      if (!match) return `- ${tags}${text}`;
+      return `- ${match[1]} ${tags}${match[2]}`;
+    })
+    .join('\n');
+
+  const text = [tagsString, tasksString, '', schedulesString].join('\n');
 
   return `\`\`\`\n${text}\n\`\`\``;
 }
