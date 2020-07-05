5011501d8c77c8ddf3a5f3fe749cf7458cfe51d4
Yuji Suzuki <y0suzuki0123@gmail.com>
2020-07-05 14:02:43 +0900

    Group TodosList

--- client/components/layout/List/List.tsx
+++ client/components/layout/List/List.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
-import { Box, Flex, Text } from 'rebass';
+import { Box, Flex } from 'rebass';
 
 import { stopPropagation } from '../../../handlers/stopPropagation';
 
 type Props = {
   isFirst?: boolean;
-  headerText?: string;
+  leftElement?: JSX.Element | null;
+  rightElement?: JSX.Element | null;
 };
 
 export const List: React.FunctionComponent<Props> = ({
-  headerText,
+  leftElement,
+  rightElement,
   isFirst = false,
   children,
 }) => {
-  const hadHeader = !!headerText;
+  const hadHeader = !!leftElement || !!rightElement;
   const extraMarginTop = isFirst ? 2 : 0;
 
   return (
     <Box
-      fontSize={2}
       mt={(hadHeader ? 3 : 1) + extraMarginTop}
       pt={hadHeader ? 2 : 0}
       sx={{ boxShadow: hadHeader ? 1 : undefined, position: 'relative' }}
@@ -26,11 +27,20 @@ export const List: React.FunctionComponent<Props> = ({
     >
       {hadHeader && (
         <Flex
-          justifyContent="flex-end"
-          sx={{ position: 'absolute', top: -2, right: 2 }}
+          color="gray"
+          fontSize={2}
+          justifyContent={
+            leftElement
+              ? rightElement
+                ? 'space-between'
+                : 'flex-start'
+              : 'flex-end'
+          }
+          sx={{ position: 'absolute', top: -2 }}
           width={1}
         >
-          <Text color="gray">{headerText}</Text>
+          {!!leftElement && <Box ml={2}>{leftElement}</Box>}
+          {!!rightElement && <Box mr={2}>{rightElement}</Box>}
         </Flex>
       )}
       {children}
