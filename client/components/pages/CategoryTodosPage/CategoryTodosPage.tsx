493a088701f73db1813a88efab58bae7e7b71f27
Yuji Suzuki <y0suzuki0123@gmail.com>
2020-07-06 23:32:31 +0900

    Extract TodoEditFormState

--- client/components/pages/CategoryTodosPage/CategoryTodosPage.tsx
+++ client/components/pages/CategoryTodosPage/CategoryTodosPage.tsx
@@ -21,10 +21,19 @@ import { SelectMode } from '../../../viewModels/SelectMode';
 import { LoadingIndicator } from '../../layout/LoadingIndicator';
 import { PageContent } from '../../layout/PageContent';
 import { TodoEditForm } from './TodoEditForm';
+import {
+  todoEditFormInitialState,
+  todoEditFormReducer,
+  todoEditFormReset,
+  todoEditFormSelectMany,
+  todoEditFormSelectOne,
+  todoEditFormSet,
+  todoEditFormToggleTag,
+} from './TodoEditFormState';
 import { TodoList } from './TodoList';
 import { TodoStatusBar } from './TodoStatusBar';
 
-function first<T>(values: T[]): T | undefined {
+export function first<T>(values: T[]): T | undefined {
   return values[0];
 }
 
@@ -51,173 +60,6 @@ const DUMMY_CHECKPOINT: RootCheckpointFragment = {
 type Props = {
   categoryId: ID;
 };
-
-type TodoEditFormState = {
-  checkpoint: RootCheckpointFragment | null;
-  selectedTodoIds: ID[];
-  status: TodoStatus | null;
-  tags: CategoryTagFragment[] | null;
-  text: string;
-};
-
-enum TodoEditFormActionType {
-  RESET = 'RESET',
-  SELECT = 'SELECT',
-  SELECT_MANY = 'SELECT_MANY',
-  SELECT_ONE = 'SELECT_ONE',
-  SET = 'SET',
-  TOGGLE_TAG = 'TOGGLE_TAG',
-}
-type TodoEditFormAction =
-  | {
-      type: TodoEditFormActionType.RESET;
-    }
-  | {
-      payload: { state: Partial<TodoEditFormState> };
-      type: TodoEditFormActionType.SET;
-    }
-  | {
-      payload: { todo: CategoryTodoFragment };
-      type: TodoEditFormActionType.SELECT;
-    }
-  | {
-      payload: { todo: CategoryTodoFragment };
-      type: TodoEditFormActionType.SELECT_ONE;
-    }
-  | {
-      payload: { todo: CategoryTodoFragment };
-      type: TodoEditFormActionType.SELECT_MANY;
-    }
-  | {
-      payload: { tag: CategoryTagFragment };
-      type: TodoEditFormActionType.TOGGLE_TAG;
-    };
-
-function todoEditFormReset(): TodoEditFormAction {
-  return {
-    type: TodoEditFormActionType.RESET,
-  };
-}
-
-function todoEditFormSelectOne(todo: CategoryTodoFragment): TodoEditFormAction {
-  return {
-    type: TodoEditFormActionType.SELECT_ONE,
-    payload: { todo },
-  };
-}
-
-function todoEditFormSelectMany(
-  todo: CategoryTodoFragment
-): TodoEditFormAction {
-  return {
-    type: TodoEditFormActionType.SELECT_MANY,
-    payload: { todo },
-  };
-}
-
-function todoEditFormToggleTag(tag: CategoryTagFragment): TodoEditFormAction {
-  return {
-    type: TodoEditFormActionType.TOGGLE_TAG,
-    payload: { tag },
-  };
-}
-
-function todoEditFormSet(
-  state: Partial<TodoEditFormState>
-): TodoEditFormAction {
-  return {
-    type: TodoEditFormActionType.SET,
-    payload: { state },
-  };
-}
-
-const todoEditFormInitialState: TodoEditFormState = {
-  checkpoint: null,
-  selectedTodoIds: [],
-  status: null,
-  tags: null,
-  text: '',
-};
-
-function todoEditFormReducer(
-  state: TodoEditFormState,
-  action: TodoEditFormAction
-): TodoEditFormState {
-  switch (action.type) {
-    case TodoEditFormActionType.RESET: {
-      return todoEditFormInitialState;
-    }
-    case TodoEditFormActionType.SET: {
-      return {
-        ...state,
-        ...action.payload.state,
-      };
-    }
-    case TodoEditFormActionType.SELECT_ONE: {
-      const { selectedTodoIds } = state;
-      const { todo } = action.payload;
-
-      const isMatch =
-        selectedTodoIds.length === 1 && first(selectedTodoIds) === todo.id;
-      if (isMatch) {
-        return todoEditFormInitialState;
-      }
-
-      return {
-        ...state,
-        checkpoint: todo.checkpoint ?? null,
-        selectedTodoIds: [todo.id],
-        status: todo.status,
-        tags: todo.tags,
-        text: todo.text,
-      };
-    }
-    case TodoEditFormActionType.SELECT_MANY: {
-      const { selectedTodoIds } = state;
-      const { todo } = action.payload;
-
-      const isSelected = !!selectedTodoIds.includes(todo.id);
-      const newSelectedTodoIds = isSelected
-        ? selectedTodoIds.filter((id) => id !== todo.id)
-        : [...selectedTodoIds, todo.id];
-      const isSingle = newSelectedTodoIds.length === 1;
-      if (isSingle) {
-        return {
-          ...state,
-          checkpoint: todo.checkpoint ?? null,
-          selectedTodoIds: newSelectedTodoIds,
-          status: todo.status,
-          tags: todo.tags,
-          text: todo.text,
-        };
-      } else {
-        return {
-          ...state,
-          checkpoint: null,
-          selectedTodoIds: newSelectedTodoIds,
-          status: null,
-          tags: null,
-          text: '',
-        };
-      }
-    }
-    case TodoEditFormActionType.TOGGLE_TAG: {
-      const { tag } = action.payload;
-      const oldTags = state.tags ?? [];
-      const has = oldTags.find((t) => t.id === tag.id);
-      const newTags = has
-        ? oldTags.filter((t) => t.id !== tag.id)
-        : [...oldTags, tag];
-      return {
-        ...state,
-        tags: newTags,
-      };
-    }
-    default: {
-      return state;
-    }
-  }
-}
 
 export const CategoryTodosPage: React.FunctionComponent<Props> = ({
   categoryId,
