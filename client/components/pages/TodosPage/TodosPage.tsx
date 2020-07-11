dbbc24e0980e48b372f4402b4d610fd5c9ccd265
Yuji Suzuki <y0suzuki0123@gmail.com>
2020-07-11 17:42:31 +0900

    Extract useTodosPageState

--- client/components/pages/TodosPage/TodosPage.tsx
+++ client/components/pages/TodosPage/TodosPage.tsx
@@ -2,73 +2,27 @@ import Head from 'next/head';
 import React from 'react';
 import { useDispatch } from 'react-redux';
 
-import { UPDATE_INTERVAL } from '../../../constants/UPDATE_INTERVAL';
 import { TodoStatus } from '../../../graphql/__generated__/baseTypes';
 import { RootCheckpointFragment } from '../../../graphql/__generated__/Checkpoint.graphql';
-import { usePageIsSyncingQuery } from '../../../graphql/__generated__/Page.graphql';
 import { RootTodoFragment } from '../../../graphql/__generated__/Todo.graphql';
 import { TodoTagFragment } from '../../../graphql/__generated__/Todo.graphql';
-import { useTodosPageQuery } from '../../../graphql/__generated__/TodosPage.graphql';
 import { useTypedSelector } from '../../../redux/useTypedSelector';
 import {
   todoEditFormReset,
   todoEditFormSelectMany,
   todoEditFormSelectOne,
   todoEditFormSet,
-  TodoEditFormState,
   todoEditFormToggleTag,
 } from '../../../state/TodoEditFormState';
 import { useTodoUsecase } from '../../../usecases/useTodoUsecase';
 import { DUMMY_CHECKPOINT } from '../../../viewModels/Checkpoint';
 import { ID } from '../../../viewModels/ID';
-import { SelectMode } from '../../../viewModels/SelectMode';
-import { isDocumentVisible } from '../../helpers/isDocumentVisible';
-import { useInterval } from '../../helpers/useInterval';
 import { LoadingIndicator } from '../../layout/LoadingIndicator';
 import { PageContent } from '../../layout/PageContent';
 import { TodoEditForm } from './TodoEditForm';
 import { TodoList } from './TodoList';
 import { TodoStatusBar } from './TodoStatusBar';
-
-function useTodosPageState(
-  categoryId: ID,
-  todoEditFormState: TodoEditFormState
-) {
-  const { data, loading, refetch } = useTodosPageQuery({
-    variables: { categoryId, categoryUUID: categoryId },
-    fetchPolicy: 'cache-and-network',
-  });
-  const { data: pageData } = usePageIsSyncingQuery();
-
-  const [now, setNow] = React.useState(() => Date.now());
-
-  useInterval(() => {
-    if (isDocumentVisible()) {
-      refetch();
-      setNow(Date.now());
-    }
-  }, UPDATE_INTERVAL);
-
-  const count = todoEditFormState.selectedTodoIds.length;
-
-  return {
-    categories: data?.categories ?? [],
-    categoryName: data?.category?.name ?? null,
-    categoryTags: data?.tags ?? [],
-    checkpoints: data?.checkpoints ?? [],
-    isLoading: !data && loading,
-    isSyncing: pageData?.page?.isSyncing ?? false,
-    now,
-    todos: data?.todos ?? [],
-    userId: data?.me?.id ?? null,
-    selectMode:
-      count === 0
-        ? SelectMode.NONE
-        : count === 1
-        ? SelectMode.SINGLE
-        : SelectMode.MULTI,
-  };
-}
+import { useTodosPageState } from './useTodosPageState';
 
 type Props = {
   categoryId: ID;
---
+++ client/components/pages/TodosPage/useTodosPageState.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import { UPDATE_INTERVAL } from '../../../constants/UPDATE_INTERVAL';
+import { usePageIsSyncingQuery } from '../../../graphql/__generated__/Page.graphql';
+import { useTodosPageQuery } from '../../../graphql/__generated__/TodosPage.graphql';
+import { TodoEditFormState } from '../../../state/TodoEditFormState';
+import { ID } from '../../../viewModels/ID';
+import { SelectMode } from '../../../viewModels/SelectMode';
+import { isDocumentVisible } from '../../helpers/isDocumentVisible';
+import { useInterval } from '../../helpers/useInterval';
+
+export function useTodosPageState(
+  categoryId: ID,
+  todoEditFormState: TodoEditFormState
+) {
+  const { data, loading, refetch } = useTodosPageQuery({
+    variables: { categoryId, categoryUUID: categoryId },
+    fetchPolicy: 'cache-and-network',
+  });
+  const { data: pageData } = usePageIsSyncingQuery();
+
+  const [now, setNow] = React.useState(() => Date.now());
+
+  useInterval(() => {
+    if (isDocumentVisible()) {
+      refetch();
+      setNow(Date.now());
+    }
+  }, UPDATE_INTERVAL);
+
+  const count = todoEditFormState.selectedTodoIds.length;
+
+  return {
+    categories: data?.categories ?? [],
+    categoryName: data?.category?.name ?? null,
+    categoryTags: data?.tags ?? [],
+    checkpoints: data?.checkpoints ?? [],
+    isLoading: !data && loading,
+    isSyncing: pageData?.page?.isSyncing ?? false,
+    now,
+    todos: data?.todos ?? [],
+    userId: data?.me?.id ?? null,
+    selectMode:
+      count === 0
+        ? SelectMode.NONE
+        : count === 1
+        ? SelectMode.SINGLE
+        : SelectMode.MULTI,
+  };
+}
