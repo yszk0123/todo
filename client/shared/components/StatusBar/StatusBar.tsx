d38461ebbfd2d39f8a2d19f91c9b313aa5a9ca74
Yuji Suzuki <y0suzuki0123@gmail.com>
2020-07-12 15:02:04 +0900

    Stop propagation in StatusBar

--- client/shared/components/StatusBar/StatusBar.tsx
+++ client/shared/components/StatusBar/StatusBar.tsx
@@ -4,6 +4,7 @@ import { Flex } from 'rebass';
 
 import { EmptyProps } from '../../../viewModels/EmptyProps';
 import { isSSR } from '../../helpers/isSSR';
+import { stopPropagation } from '../../view_helpers/stopPropagation';
 
 export const StatusBar: React.FunctionComponent<EmptyProps> = ({
   children,
@@ -20,6 +21,7 @@ export const StatusBar: React.FunctionComponent<EmptyProps> = ({
       justifyContent="space-between"
       p={2}
       sx={{ position: 'sticky', zIndex: 2, bottom: 0 }}
+      onClick={stopPropagation}
     >
       {children}
     </Flex>,
