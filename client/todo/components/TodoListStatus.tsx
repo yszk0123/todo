5a79cd1c536646164852f1a82704008afc3a744e
Yuji Suzuki <y0suzuki0123@gmail.com>
2020-07-15 23:29:00 +0900

    Extract TodoStatusIcon

--- client/todo/components/TodoListStatus.tsx
+++ client/todo/components/TodoListStatus.tsx
@@ -2,29 +2,13 @@ import React from 'react';
 
 import { ListIcon } from '../../shared/components/List';
 import { TodoStatus } from '../../shared/graphql/__generated__/baseTypes';
-import { TodoStatusDoneIcon } from './TodoStatusDoneIcon';
-import { TodoStatusInProgressIcon } from './TodoStatusInProgressIcon';
-import { TodoStatusTodoIcon } from './TodoStatusTodoIcon';
-import { TodoStatusWaitingIcon } from './TodoStatusWaitingIcon';
-
-const Status: React.FunctionComponent<{ status: TodoStatus }> = ({
-  status,
-}) => {
-  switch (status) {
-    case TodoStatus.Todo:
-      return <TodoStatusTodoIcon />;
-    case TodoStatus.InProgress:
-      return <TodoStatusInProgressIcon />;
-    case TodoStatus.Waiting:
-      return <TodoStatusWaitingIcon />;
-    case TodoStatus.Done:
-      return <TodoStatusDoneIcon />;
-  }
-};
+import { TodoStatusIcon } from './TodoStatusIcon';
 
 export const TodoListStatusIcon: React.FunctionComponent<{
   onClick: () => void;
   status: TodoStatus;
 }> = ({ onClick, status }) => {
-  return <ListIcon icon={<Status status={status} />} onClick={onClick} />;
+  return (
+    <ListIcon icon={<TodoStatusIcon status={status} />} onClick={onClick} />
+  );
 };
---
+++ client/todo/components/TodoStatusIcon.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { TodoStatus } from '../../shared/graphql/__generated__/baseTypes';
+import { TodoStatusDoneIcon } from './TodoStatusDoneIcon';
+import { TodoStatusInProgressIcon } from './TodoStatusInProgressIcon';
+import { TodoStatusTodoIcon } from './TodoStatusTodoIcon';
+import { TodoStatusWaitingIcon } from './TodoStatusWaitingIcon';
+
+export const TodoStatusIcon: React.FunctionComponent<{
+  status: TodoStatus;
+}> = ({ status }) => {
+  switch (status) {
+    case TodoStatus.Todo:
+      return <TodoStatusTodoIcon />;
+    case TodoStatus.InProgress:
+      return <TodoStatusInProgressIcon />;
+    case TodoStatus.Waiting:
+      return <TodoStatusWaitingIcon />;
+    case TodoStatus.Done:
+      return <TodoStatusDoneIcon />;
+  }
+};
