3f7a029f63a5f8b5cab6a89545972ed173dda5a3
Yuji Suzuki <y0suzuki0123@gmail.com>
2020-07-18 18:12:54 +0900

    Avoid checking elements injected by nextjs

--- client/bootstrap/injectReactAxe.tsx
+++ client/bootstrap/injectReactAxe.tsx
@@ -8,6 +8,8 @@ const TIMEOUT = 1000;
 export function injectReactAxe(): void {
   if (process.env.NODE_ENV !== 'production' && !isSSR()) {
     const axe = require('react-axe');
-    axe(React, ReactDOM, TIMEOUT);
+    axe(React, ReactDOM, TIMEOUT, undefined, {
+      exclude: [['#__next-prerender-indicator'], ['#__next-build-watcher']],
+    });
   }
 }
