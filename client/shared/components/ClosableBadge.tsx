085a98ac80275f9ab3abea76a8e64673f52be1f9
Yuji Suzuki <y0suzuki0123@gmail.com>
2020-07-19 01:31:51 +0900

    Fix a11y issues

--- client/shared/components/ClosableBadge.tsx
+++ client/shared/components/ClosableBadge.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Color, getColorCode } from '../../view_models/Color';
+import { Color, getColorCode, isLightColor } from '../../view_models/Color';
 import { Badge } from './Badge';
 import { CloseIcon } from './CloseIcon';
 
@@ -10,11 +10,13 @@ export const ClosableBadge: React.FunctionComponent<{
   text: string;
 }> = ({ color, onClick, text }) => {
   const colorCode = getColorCode(color);
+  const lightColor = isLightColor(color);
 
   return (
     <Badge
       color={colorCode}
       icon={<CloseIcon />}
+      isReverseColor={lightColor}
       text={text}
       onClick={onClick}
     />
