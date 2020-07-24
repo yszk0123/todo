6dbbedf35492212ea135ba5355be5fcbeac71751
Yuji Suzuki <y0suzuki0123@gmail.com>
2020-07-24 20:22:54 +0900

    Fix outline rendering issue in webkit

--- client/bootstrap/components/GlobalStyle.tsx
+++ client/bootstrap/components/GlobalStyle.tsx
@@ -4,10 +4,22 @@ import { FONT_FAMILY } from '../../shared/theme/theme';
 import { EmptyProps } from '../../view_models/EmptyProps';
 
 export const GlobalStyle: React.FunctionComponent<EmptyProps> = () => {
+  React.useEffect(() => {
+    navigator.userAgent.search('Safari') >= 0;
+  }, []);
+
   return (
     <style global jsx>{`
       html {
         font-family: ${FONT_FAMILY};
+      }
+
+      /* FIXME: In webkit, outline is not clear completely after focusout */
+      @media only screen and (max-device-width: 768px) {
+        body *:focus {
+          outline: 2px solid skyblue;
+          outline-offset: -2px;
+        }
       }
     `}</style>
   );
