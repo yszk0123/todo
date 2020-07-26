2632cdec7e875b1817478a3c15d3f309987748fc
Yuji Suzuki <y0suzuki0123@gmail.com>
2020-07-27 00:04:54 +0900

    Rename RESET to UNSET

--- client/view_models/Checkpoint.ts
+++ client/view_models/Checkpoint.ts
@@ -4,6 +4,6 @@ import { toDateTime } from './DateTime';
 // FIXME
 export const DUMMY_CHECKPOINT: RootCheckpointFragment = {
   id: '__DUMMY__',
-  name: 'RESET',
+  name: 'UNSET',
   endAt: toDateTime('2000-01-01'),
 };
